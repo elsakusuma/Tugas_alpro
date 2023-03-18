c73c54c2eea4f9fba867173d93c87c076c282b44
unknown <elsakusuma261@gmail.com>
2023-03-18 23:38:35 +0700

    first commit

---
+++ programdatadiri.java
@@ -0,0 +1,21 @@
+public class programdatadiri {
+    public static void main(String[] args) {
+        //deklarasi Variabel
+        String nama, alamat;
+        int umur;
+        double tinggi;
+
+        //pengisian Variabel
+        nama = "Elsa Kusuma";
+        alamat = "Perum. Griya cileungsi 05 Blok A14 No.01";
+        umur = 19;
+        tinggi = 167.5;
+
+        //menampilkan
+        System.out.println("NAMA\t :" + nama); 
+        System.out.println("ALAMAT\t :" + alamat);
+        System.out.println("UMUR\t :" + umur);
+        System.out.println("TINGGI\t :" + tinggi);
+    }
+    
+}
